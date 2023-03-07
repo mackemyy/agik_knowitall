b9cc6b1e482492c831b7db6764031ceb12d16367
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2023-03-07 09:12:33 +0800

    Added config game.js file

---
+++ game.js
@@ -0,0 +1,24 @@
+var config = {
+    type: Phaser.AUTO,
+    backgroundColor: 0x000,
+    scene: [],
+    pixelArt: true,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: false,
+        }
+    },
+    dom: {
+        createContainer: true
+    },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        max: {width:1920, height: 1080},
+        width: 1920,
+        height: 1080
+    },
+}
+
+var game = new Phaser.Game(config);
