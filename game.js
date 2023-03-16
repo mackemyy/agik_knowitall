d27900b5c902ba833b5f06d33b584d289b0cd8cd
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2023-03-16 12:44:02 +0800

    Added necessary files for import

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 var config = {
     type: Phaser.AUTO,
     backgroundColor: 0x0C2238,
-    scene: [LoadingScene, LoadAllScenes],
+    scene: [LoadingScene, LoadAllScenes, IntroGame, PlayLogofy],
     pixelArt: true,
     physics: {
         default: "arcade",
