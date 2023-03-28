c66d90cb5ee11ae0876ad8e9af8c9757b6f23311
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2023-03-28 12:37:50 +0800

    Modify LevelMap

--- LevelMap.js
+++ LevelMap.js
@@ -4,13 +4,26 @@ class LevelMap extends Phaser.Scene{
     }
 
     preload() {
-        this.load.image("levelMap", "assets/bg/level_map.png");
+        this.load.image("levelMapBg", "assets/bg/level_map_bg.png");
+        this.load.image("level1", "assets/objects/level1.png");
+        this.load.image("level2", "assets/objects/level2.png");
+        this.load.image("miniGame", "assets/objects/miniGame.png");
      }
 
     create() { 
-        this.levelMap = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2, "levelMap")
+        this.levelMapBg = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2, "levelMapBg");
+        this.level1 = this.add.image(this.cameras.main.width/2 - 590, this.cameras.main.height/2 + 90, "level1")
             .setInteractive({useHandCursor: true})
-            .on('pointerdown', () => this.scene.start('playLogofy'))
+            .setScale(0.9)
+            .on('pointerdown', () => this.scene.start('playLogofy'));
+        this.miniGame = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2 + 265, "miniGame")
+            .setInteractive({useHandCursor: true})
+            .setScale(0.8)
+            .on('pointerdown', () => console.log('miniGame'));
+        this.level2 = this.add.image(this.cameras.main.width/2 + 590, this.cameras.main.height/2 + 90, "level2")
+            .setInteractive({useHandCursor: true})
+            .setScale(0.9)
+            .on('pointerdown', () => console.log('level2'));
     }
 
     update() { }
