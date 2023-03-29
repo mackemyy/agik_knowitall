e2550db12dc989cc55ae3818686a684c4f6dc3be
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2023-03-29 12:10:56 +0800

    Add next button in Game Objective

--- LogofyGame.js
+++ LogofyGame.js
@@ -10,6 +10,7 @@ class PlayLogofy extends Phaser.Scene {
         this.load.image("playbtn", "assets/buttons/playbtn.png");
         this.load.image("hrDP", "assets/objects/hrDP.png");
         this.load.image("hrNarrateCtr", "assets/objects/hrNarrateContainer.png");
+        this.load.image("nextBtn", "assets/buttons/nextButton.png");
 
     }
 
@@ -56,6 +57,7 @@ class PlayLogofy extends Phaser.Scene {
         this.logofyTxt.alpha = 0;
         this.letsDesignTxt.alpha = 0;
         this.gameObjCtr = this.add.image(config.scale.width/2, config.scale.height/2 - 300, "gameObjCtr");
+        
 
         this.gameObjTitleTxt = this.add.text(config.scale.width/2 - 295, config.scale.height/2 - 350, "game objective", { fontFamily: '"Typesauce"', fill: '#FFFFFF', fontSize: '72px', align: "center", });
         
@@ -89,18 +91,25 @@ class PlayLogofy extends Phaser.Scene {
             duration: 15000,
             // duration: 100,
             ease: Phaser.Math.Easing.Sine.Out,
-            onComplete: function() {
-                this.gameObjCtr.setVisible(false);
-                this.gameObjTitleTxt.setVisible(false);
-                this.gameTitleCtr.setVisible(false);
-                this.groupGameObjTxt.setVisible(false);    
+            onComplete: function() {    
                 this.showHRNarrate();
             },
             callbackScope: this,
         });
+        this.nextBtn = this.add.image(this.cameras.main.width/2 + 800, this.cameras.main.height/2 + 420, "nextBtn")
+            .setInteractive({useHandCursor: true})
+            .setScale(0.8)
+            .on('pointerdown', () => this.showHRNarrate())
+            .on('pointerover', () => this.nextBtn.setScale(1))
+            .on('pointerout', () => this.nextBtn.setScale(0.8))
      }
 
      showHRNarrate() {
+        this.gameObjCtr.setVisible(false);
+        this.gameObjTitleTxt.setVisible(false);
+        this.gameTitleCtr.setVisible(false);
+        this.groupGameObjTxt.setVisible(false);
+        this.nextBtn.setVisible(false);
         this.hrNarrateCtr = this.add.image(config.scale.width/2, config.scale.height/2 + 250, "hrNarrateCtr");
         this.hrDP = this.add.image(config.scale.width/2 - 465, config.scale.height/2 + 240, "hrDP");
         this.playbtn = this.add.image(config.scale.width/2 + 500, config.scale.height/2 + 350, "playbtn")
