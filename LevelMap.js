4097eab8da1421ea57d57de19108a87390a01327
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2023-03-29 12:08:42 +0800

    Modify assets for each map and its scale

--- LevelMap.js
+++ LevelMap.js
@@ -14,15 +14,15 @@ class LevelMap extends Phaser.Scene{
         this.levelMapBg = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2, "levelMapBg");
         this.level1 = this.add.image(this.cameras.main.width/2 - 590, this.cameras.main.height/2 + 90, "level1")
             .setInteractive({useHandCursor: true})
-            .setScale(0.9)
+            .setScale(0.6)
             .on('pointerdown', () => this.scene.start('playLogofy'));
         this.miniGame = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2 + 265, "miniGame")
             .setInteractive({useHandCursor: true})
-            .setScale(0.8)
+            .setScale(0.5)
             .on('pointerdown', () => console.log('miniGame'));
         this.level2 = this.add.image(this.cameras.main.width/2 + 590, this.cameras.main.height/2 + 90, "level2")
             .setInteractive({useHandCursor: true})
-            .setScale(0.9)
+            .setScale(0.6)
             .on('pointerdown', () => console.log('level2'));
     }
 
